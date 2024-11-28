869ffe5c3da0739be7ce5b4cb80448dde83d700d
Hrishi93 <sonumahure@gmail.com>
2024-11-28 11:30:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FormComponent from './comps/FormComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Form Handling Demo</h1>
+        <FormComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
